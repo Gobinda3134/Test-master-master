50b90d6a07367e954c83c6b4d463b7e190709428
Gobinda3134 <bprajwol10@gmail.com>
2020-02-14 18:15:08 -0500

    api url is add

--- app/src/main/java/com/example/e_softwarica/url/Url.java
+++ app/src/main/java/com/example/e_softwarica/url/Url.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Url {
-    public static final String BASE_URL ="";
+    public static final String BASE_URL ="http://digitalsoftwarica.000webhostapp.com/";
     public static  String token="";
     public static Retrofit getInstance(){
         Retrofit retrofit = new Retrofit.Builder().baseUrl(Url.BASE_URL)
